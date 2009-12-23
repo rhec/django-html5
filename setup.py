2c3d2d65ddd5dc375bc1355e9811f29b9806b2ff
Robert Eanes <github@robsinbox.com>
2009-12-23 00:19:31 -0500

    more setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='html5',
+    version='0.1',
+    description='Django HTML5 support',
+    author='Robert Eanes',
+    author_email='django@robsinbox.com',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
