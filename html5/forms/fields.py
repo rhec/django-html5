ead5aa36dde2d4becdcc0b3abf3df931c21c8649
Robert Eanes <github@robsinbox.com>
2009-12-23 01:07:13 -0500

    implemented IntegerField using the NumberInput widget

---
+++ html5/forms/fields.py
@@ -0,0 +1,19 @@
+from django.forms.fields import IntegerField as DjangoIntegerField
+
+from html5.widgets import NumberInput
+
+class IntegerField(DjangoIntegerField):
+    widget = NumberInput
+
+    def widget_attrs(self, widget):
+        """
+        Given a Widget instance (*not* a Widget class), returns a dictionary of
+        any HTML attributes that should be added to the Widget, based on this
+        Field.
+        """
+        attrs = {}
+        if self.min_value:
+            attrs['min'] = self.min_value
+        if self.max_value:
+            attrs['max'] = self.max_value
+        return attrs
