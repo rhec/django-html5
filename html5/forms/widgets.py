c6f98e01c7e126a581f46d53d5a212f0b7918ccb
Robert Eanes <github@robsinbox.com>
2010-01-13 16:26:46 -0500

    Create HTML5Input that inherits from django's Input instead of TextInput. Added Date type inputs and optional autofocus JS fallback

--- html5/forms/widgets.py
+++ html5/forms/widgets.py
@@ -2,22 +2,59 @@
 HTML5 input widgets.
 TODO: Date widgets
 """
-from django.forms.widgets import TextInput
+from django.forms.widgets import Input
 
-class EmailInput(TextInput):
+class HTML5Input(Input):
+    use_autofocus_fallback = False
+    
+    def render(self, *args, **kwargs):
+        rendered_string = super(HTML5Input, self).render(*args, **kwargs)
+        if self.use_autofocus_fallback:
+            rendered_string += """<script>
+if (!("autofocus" in document.createElement("input"))) {
+  document.getElementById("q").focus();
+}
+</script>"""
+        return rendered_string
+
+class TextInput(HTML5Input):
+    input_type = 'text'
+
+class EmailInput(HTML5Input):
     input_type = 'email'
     
-class URLInput(TextInput):
+class URLInput(HTML5Input):
     input_type = 'url'
 
-class SearchInput(TextInput):
+class SearchInput(HTML5Input):
     input_type = 'search'
+
+class ColorInput(HTML5Input):
+    """
+    Not supported by any browsers at this time (Jan. 2010).
+    """
+    input_type = 'color'
     
-class NumberInput(TextInput):
+class NumberInput(HTML5Input):
     input_type = 'number'
     
 class RangeInput(NumberInput):
     input_type = 'range'
     
+class DateInput(HTML5Input):
+    input_type = 'date'
     
+class MonthInput(HTML5Input):
+    input_type = 'month'
 
+class WeekInput(HTML5Input):
+    input_type = 'week'
+
+class TimeInput(HTML5Input):
+    input_type = 'time'
+
+class DateTimeInput(HTML5Input):
+    input_type = 'datetime'
+
+class DateTimeLocalInput(HTML5Input):
+    input_type = 'datetime-local'
